3a3994696add8aff77cf27f576e4a51a0f7ff641
Revention <25434575+Revention@users.noreply.github.com>
2025-04-22 11:23:39 +0200

    Added an experimental function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,16 @@
-alert("Show me! - alert in the application javascript file");
+// Javascript application for calculating values from the unconventional calculator webpage
+// NOTE: This script is under development and might not work as expected
+
+// const declarations
+const defaultResult = 0;
+let currentResult = defaultResult;
+let calculatorDescription = "(" + defaultResult + " 10) * 3 / 2 - 1";
+
+// const buttonAdd;
+// const buttonSubtrackt;
+// const buttonMultiply;
+// const buttonDivide;
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+outputResult(currentResult, calculatorDescription);
