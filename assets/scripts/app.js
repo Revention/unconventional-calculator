994191565739fb3eaa374b05be16ab5f1c7727ae
Revention <25434575+Revention@users.noreply.github.com>
2025-05-04 10:01:48 +0200

    Function for adding two numbers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,27 @@
 // Javascript application for calculating values from the unconventional calculator webpage
 // NOTE: This script is under development and might not work as expected
 
-// const declarations
+// Declarations
 const defaultResult = 0;
 let currentResult = defaultResult;
-let calculatorDescription = "(" + defaultResult + " 10) * 3 / 2 - 1";
+let calculatorDescription = `( ${defaultResult} )`;
 
-// const buttonAdd;
-// const buttonSubtrackt;
-// const buttonMultiply;
-// const buttonDivide;
+// Main
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+addBtn.addEventListener("click", calculator);
 
-outputResult(currentResult, calculatorDescription);
+// Functions
+
+// function for adding the user input value to the currentresult
+function addNumber(val1, val2) {
+  calculatorDescription = `${val1} + ${val2}`;
+  currentResult = val1 + val2;
+
+  return currentResult, calculatorDescription;
+}
+
+// function for starting calculations and checking input values
+function calculator() {
+  addNumber(currentResult, Number(usrInput.value));
+  outputResult(currentResult, calculatorDescription);
+}
