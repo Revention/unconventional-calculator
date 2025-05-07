64fa453b2d5f2724e22c00e43cae87c6efb4c488
Revention <25434575+Revention@users.noreply.github.com>
2025-05-07 12:18:17 +0200

    All the functions added for operation buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,21 +7,81 @@ let currentResult = defaultResult;
 let calculatorDescription = `( ${defaultResult} )`;
 
 // Main
-
-addBtn.addEventListener("click", calculator);
+addBtn.addEventListener("click", addOperator);
+subtractBtn.addEventListener("click", substractOperator);
+multiplyBtn.addEventListener("click", multiplyOperator);
+divideBtn.addEventListener("click", divideOperator);
 
 // Functions
 
+// function for getting the userinput value
+function getUserInputValue() {
+  const number = parseInt(usrInput.value);
+  return number;
+}
+
+// function for selectiong operation based on button
+function operatorSelect() {
+  const operator = addBtn.value;
+  return operator;
+}
+
+function addOperator() {
+  operator = "+";
+  console.log("Operator for adding selected", operator);
+  calculator();
+}
+
+function substractOperator() {
+  operator = "-";
+  console.log("Operator for substracting selected", operator);
+  calculator();
+}
+
+function multiplyOperator() {
+  operator = "*";
+  console.log("Operator for mulitplication selected", operator);
+  calculator();
+}
+
+function divideOperator() {
+  operator = "/";
+  console.log("Operator for division selected", operator);
+  calculator();
+}
 // function for adding the user input value to the currentresult
-function addNumber(val1, val2) {
-  calculatorDescription = `${val1} + ${val2}`;
-  currentResult = val1 + val2;
+function calculation(opr, val1, val2) {
+  calculatorDescription = `${val1} ${opr} ${val2}`;
+
+  switch (opr) {
+    case "+":
+      currentResult = val1 + val2;
+      console.log("switch add");
+      break;
+    case "-":
+      currentResult = val1 - val2;
+      console.log("switch substract");
+      break;
+    case "*":
+      currentResult = val1 * val2;
+      console.log("switch multiply");
+      break;
+    case "/":
+      currentResult = val1 / val2;
+      console.log("switch divide");
+      break;
+    default:
+      console.log("selection failed");
+  }
 
   return currentResult, calculatorDescription;
 }
 
-// function for starting calculations and checking input values
+// function for starting calculations and converting input values
 function calculator() {
-  addNumber(currentResult, Number(usrInput.value));
+  const num = getUserInputValue();
+  const opr = operator;
+  console.log(opr, currentResult, num);
+  calculation(opr, currentResult, num);
   outputResult(currentResult, calculatorDescription);
 }
