415ff1ed30c96ef11646bb43c6ae2036ff674f68
Revention <25434575+Revention@users.noreply.github.com>
2025-05-09 11:55:57 +0200

    Code refactoring and optimization

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,75 +13,66 @@ multiplyBtn.addEventListener("click", multiplyOperator);
 divideBtn.addEventListener("click", divideOperator);
 
 // Functions
+function addOperator() {
+  const button = addBtn;
+  operatorSelect(button);
+}
 
-// function for getting the userinput value
+function substractOperator() {
+  const button = subtractBtn;
+  operatorSelect(button);
+}
+
+function multiplyOperator() {
+  const button = multiplyBtn;
+  operatorSelect(button);
+}
+
+function divideOperator() {
+  const button = divideBtn;
+  operatorSelect(button);
+}
+
+// operator selection based on html-button pressed (index.html vendor.js)
+function operatorSelect(button) {
+  operator = button.innerText;
+  console.log(`operator.operatorSelect is ${operator}`);
+  calculator(operator);
+}
+
+// get the userinput value from html field through vendor.js
 function getUserInputValue() {
   const number = parseInt(usrInput.value);
   return number;
 }
 
-// function for selectiong operation based on button
-function operatorSelect() {
-  const operator = addBtn.value;
-  return operator;
+// calculations router
+function calculator(operator) {
+  const num = getUserInputValue();
+  calculation(operator, currentResult, num);
+  outputResult(currentResult, calculatorDescription);
 }
 
-function addOperator() {
-  operator = "+";
-  console.log("Operator for adding selected", operator);
-  calculator();
-}
-
-function substractOperator() {
-  operator = "-";
-  console.log("Operator for substracting selected", operator);
-  calculator();
-}
-
-function multiplyOperator() {
-  operator = "*";
-  console.log("Operator for mulitplication selected", operator);
-  calculator();
-}
-
-function divideOperator() {
-  operator = "/";
-  console.log("Operator for division selected", operator);
-  calculator();
-}
-// function for adding the user input value to the currentresult
+// preform calculation based on userinputvalue and operator
 function calculation(opr, val1, val2) {
   calculatorDescription = `${val1} ${opr} ${val2}`;
 
   switch (opr) {
     case "+":
       currentResult = val1 + val2;
-      console.log("switch add");
       break;
     case "-":
       currentResult = val1 - val2;
-      console.log("switch substract");
       break;
     case "*":
       currentResult = val1 * val2;
-      console.log("switch multiply");
       break;
     case "/":
       currentResult = val1 / val2;
-      console.log("switch divide");
       break;
     default:
       console.log("selection failed");
   }
 
   return currentResult, calculatorDescription;
-}
-
-// function for starting calculations and converting input values
-function calculator() {
-  const num = getUserInputValue();
-  const opr = operator;
-  console.log(opr, currentResult, num);
-  calculation(opr, currentResult, num);
-  outputResult(currentResult, calculatorDescription);
 }
